6eed12f0c5bd3e3b0c02d389b4e780f396e7e3e7
Kitamura Satoshi <with.no.parachute@gmail.com>
2013-03-04 00:29:26 +0900

    add lessen 2

---
+++ src/main/scala/Lessen2.scala
@@ -0,0 +1,78 @@
+package main.scala
+
+/**
+ * Lessen2.
+ *
+ * http://vipprog.net/wiki/exercise.html
+ *
+ * FizzBuzz
+ *
+ * なんか偉い人が考えた問題
+ *
+ * ルールは以下の通り
+ *
+ * - 1から順番に数を表示する
+ * - その数が3で割り切れるなら"Fizz"、5で割り切れるなら"Buzz"、両方で割り切れるなら"FizzBuzz"と表示する
+ * - 要するに"1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz ･･･"と出力される
+ * - プログラマかどうかがわかるんだとさ
+ *
+ * 実行例
+ * 1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz 31 32 Fizz 34
+ */
+object Lessen2 {
+  def main(args: Array[String]) {
+    //for (i <- 1 to 99) println(fizzBuzz(i))
+    //for (i <- 1 to 99) println(fizzBuzz2(i))
+    fizzBuzz3(99)
+  }
+
+  /**
+   * basic idea 1.
+   *
+   * @param i
+   * @return
+   */
+  def fizzBuzz(i: Int): Any = {
+    val fizz = i % 3 == 0
+    val buzz = i % 5 == 0
+
+    if (fizz && buzz) "FizzBuzz"
+    else if (fizz) "Fizz"
+    else if (buzz) "Buzz"
+    else i
+  }
+
+  /**
+   * basic idea 2.
+   *
+   * @param i
+   * @return
+   */
+  def fizzBuzz2(i: Int): Any = {
+    var s = ""
+
+    if (i % 3 == 0) s += "Fizz"
+    if (i % 5 == 0) s += "Buzz"
+
+    if (s.isEmpty) i else s
+  }
+
+  /**
+   * functional programming style.
+   *
+   * これは思いつかないわー
+   * http://yuroyoro.hatenablog.com/entry/20100317/1268819400
+   *
+   * @param i
+   */
+  def fizzBuzz3(i: Int) = {
+    1 to i
+  } map {
+    case i if i % 15 == 0 => "FizzBuzz"
+    case i if i % 3 == 0 => "Fizz"
+    case i if i % 5 == 0 => "Buzz"
+    case i => i
+  } foreach {
+    s => println(s)
+  }
+}
