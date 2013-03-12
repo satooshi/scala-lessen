b1689b3606a52ba194a7979435c313daafb3db51
Kitamura Satoshi <with.no.parachute@gmail.com>
2013-03-12 18:20:36 +0900

    improve lessen4

--- src/main/scala/Lessen4.scala
+++ src/main/scala/Lessen4.scala
@@ -1,5 +1,7 @@
 package main.scala
 
+import collection.mutable
+
 /**
  * Lessen3.
  *
@@ -21,30 +23,13 @@ package main.scala
  */
 object Lessen4 {
   def main(args: Array[String]) {
-    val i = 10000
-    // list of prime numbers ranging from 2 to i
-    println(PrimeNumber.getList(i))
+    val i = 100
 
-    // whether argument i is the prime number
-    //println(PrimeNumber.isPrimeNumber(i))
+    // list prime numbers ranging from 2 to i
+    println(PrimeNumber.toList(i))
 
-    println("hoge")
-
-    timer(PrimeNumber.isPrimeNumber2(i))
-    timer(PrimeNumber.isPrimeNumber(i))
-
-    timer(PrimeNumber.isPrimeNumber2(i))
-    timer(PrimeNumber.isPrimeNumber(i))
-
-    timer(PrimeNumber.isPrimeNumber2(i))
-    timer(PrimeNumber.isPrimeNumber(i))
-  }
-
-  def timer(f: => Unit) {
-    System.gc
-    val start = System.nanoTime
-    f
-    println("Elapsed time: %d".format(System.nanoTime - start))
+    // whether i is prime number
+    println(PrimeNumber.contains(i))
   }
 }
 
@@ -55,33 +40,39 @@ object PrimeNumber {
    * @param i Target number.
    * @return true if the target number is the prime number, false otherwise.
    */
-  def isPrimeNumber(i: Int) = getList(i).contains(i)
+  def contains(i: Int) = i match {
+    case i if i <= 1 => false
+    case i if i == 2 => true
+    //case _           => toList(i).contains(i)
+    // この方が速かった
+    case _           => Stream.range(2, i).forall(i % _ != 0)
+  }
 
   /**
-   * Return prime numbers list.
+   * Return prime numbers.
    *
-   * @param max Maximum number to search prime numbers.
+   * @param max Maximum number of the list.
    * @return Prime numbers.
    */
-  def getList(max: Int) = max match {
+  def toList(max: Int) = max match {
     case max if max <= 1 => List()
     case max if max == 2 => List(2)
     // step 1
     // 整数を最初の素数である 2 から昇順で探索リストに羅列する。
     // step 2
     // リストの先頭の数を素数リストに記録する。
-    case _ => search(List(2), 2 to max)
+    case _ => eratosthenesSieve(List(2), 2 to max)
   }
 
   /**
-   * Return prime numbers.
+   * Run Eratosthenes's sieve algorithm.
    *
    * @param primes Prime numbers.
-   * @param range  Search range.
+   * @param range  Candidate range.
    * @return Prime numbers.
    */
   private
-  def search(primes: List[Int], range: Seq[Int]): List[Int] = range.filter {
+  def eratosthenesSieve(primes: List[Int], range: Seq[Int]): List[Int] = range.filter {
     // step 3
     // 前のステップで素数リストに加えられた数の全ての倍数を、探索リストから削除する。
     _ % primes.last != 0
@@ -89,16 +80,9 @@ object PrimeNumber {
     // step 4
     // 探索リストの最大値が素数リストの最大値の平方よりも小さい場合、素数リストおよび探索リストに残っている数が素数となる。
     // 探索リストの最大値が素数リストの最大値の平方よりも大きい場合、ステップ 2 に戻る。
-    case candidates if candidates.max <= math.pow(primes.max, 2) => primes ::: candidates.toList
+    case candidates if candidates.max <= math.pow(primes.max, 2) => primes ++ candidates
     // step 2
     // リストの先頭の数を素数リストに記録する。
-    case candidates => search(primes ::: List(candidates.head), candidates)
-  }
-
-
-  def isPrimeNumber2(i: Int) = i match {
-    case i if i <= 1 => false
-    case i if i == 2 => true
-    case _           => 2 to i forall { i % _ != 0 }
+    case candidates => eratosthenesSieve(primes ::: List(candidates.head), candidates)
   }
 }
